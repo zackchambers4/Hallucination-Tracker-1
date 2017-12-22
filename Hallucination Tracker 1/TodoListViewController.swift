a375c45281e3f09001620b54f55969a259e6e34b
Zack Chambers <zackchambers@Zacks-MacBook-Pro.local>
2017-12-22 13:45:22 -0800

    Todo list with datasource and delegate methods

---
+++ Hallucination Tracker 1/TodoListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Hallucination Tracker 1
+//
+//  Created by Zack Chambers on 12/22/17.
+//  Copyright © 2017 Phitas Technologies. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["12/21/17", "12/23/17", "1/1/18"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
