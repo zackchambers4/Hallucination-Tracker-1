02a3dcda05cadc38f7276b62b8d43efff060adc8
Zack Chambers <zackchambers@Zacks-MacBook-Pro.local>
2017-12-31 13:04:37 -0800

    convert to MVC design pattern

--- Hallucination Tracker 1/Controllers/TodoListViewController.swift
+++ Hallucination Tracker 1/Controllers/TodoListViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["12/21/17", "12/23/17", "1/1/18"]
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let newItem = Item()
+        newItem.title = "Date of Hallucination:"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Date of Hallucination:"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Date of Hallucination:"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+        
         
     }
     
@@ -25,7 +45,12 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+    
         
         return cell
     }
@@ -33,12 +58,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+       
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -52,7 +74,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Date", style: .default) { (action) in
             // What happens when the user clicks the Add Date button on screen
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
---
+++ Hallucination Tracker 1/Data Model/item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Hallucination Tracker 1
+//
+//  Created by Zack Chambers on 12/31/17.
+//  Copyright © 2017 Phitas Technologies. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
+
